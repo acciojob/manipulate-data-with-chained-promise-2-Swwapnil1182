4ed1d5baf3557b135aa3769c380b23002cbd1e71
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-22 12:53:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your JS code here. If required.
+
+function initialPromise() { 
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([1, 2, 3, 4])
+        }, 3000)
+    })
+}
+
+function filterEvenPromise(arr) {
+    return new Promise((resolve) => {
+        const evenNumbers = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                evenNumbers.push(arr[i])
+            }
+        }
+
+        setTimeout(() => {
+            document.getElementById('output').textContent = evenNumbers.join(",")
+            resolve(evenNumbers)
+        }, 1000)
+    })
+}
+
+function multiplyPromise(arr){
+    return new Promise((resolve)=>{
+        const multiplied=[];
+        for(let i=0;i<arr.length;i++){
+            multiplied.push(arr[i]*2)
+        }
+
+        setTimeout(()=>{
+            document.getElementById('output').textContent = multiplied.join(",")
+                resolve(multiplied)
+        },2000)
+    })
+}
+
+initialPromise()
+    .then(result => filterEvenPromise(result))
+    .then(filtered => multiplyPromise(filtered))
